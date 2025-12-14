70d65c5580995c6aef063d30bdbbf2829517736f
Marthala vishnu vardhan reddy <vishnu_reddy220@yahoo.com>
2025-12-14 23:30:43 +0100

    fix logo

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import Navigation from './Navigation';
 
 export default function Header() {
@@ -8,10 +9,14 @@ export default function Header() {
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
           <Link href="/" className="flex items-center space-x-3">
-            <div className="w-12 h-12 bg-temple-maroon rounded-full flex items-center justify-center">
-              <span className="text-temple-gold font-serif font-bold text-xl">
-                SVT
-              </span>
+            <div className="relative w-12 h-12">
+              <Image
+                src="/images/SVT_Logo_Red.png"
+                alt="Sri Venkateshwara Temple Stuttgart"
+                fill
+                className="object-contain"
+                priority
+              />
             </div>
             <div className="hidden sm:block">
               <h1 className="text-lg font-serif text-temple-dark-red leading-tight">
